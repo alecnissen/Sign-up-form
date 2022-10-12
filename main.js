ebd7653e2c1073ef2a73b7f07c6e09123f1c167f
alecnissen <alecnissen@yahoo.com>
2022-10-12 11:24:57 -0400

    fixed up media queries to make sure header text and logo are properly aligned, played around with font-sizes, mostly just experimenting to make sure the page looks nice on different dimensions and devices

--- main.js
+++ main.js
@@ -1,114 +1,3 @@
-// $(document).ajaxStart(function() {
-//     Pace.restart();
-// });
-// $(document).ready(function() {
-//   $('input[type=password]').keyup(function() {
-//       $('#password_confirmation').on('keyup',function(){
-//       $('#pswd_info').hide();
-//   });
-//       // keyup event code here
-//   });
-//   $('input[type=password]').focus(function() {
-//       // focus code here
-//   });
-//   $('input[type=password]').blur(function() {
-
-//     $('#password').removeClass('valid');
-//       // blur code here
-//   });
-//    $('input[type=password]').keyup(function() {
-//       // keyup code here
-//   }).focus(function() {
-//       // focus code here
-
-//   }).blur(function() {
-//       // blur code here
-//   });
-
-//   $('#password').keyup(function() {
-//   // keyup code here
-//   var pswd = $(this).val();
-//   //validate the length
-//   if ( pswd.length < 8 ) {
-//       $('#length').removeClass('valid').addClass('invalid');
-//   } else {
-//       $('#length').removeClass('invalid').addClass('valid');
-//   }
-
-//   //validate letter
-//   if ( pswd.match(/[a-z]/) ) {
-//       $('#letter').removeClass('invalid').addClass('valid');
-//   } else {
-//       $('#letter').removeClass('valid').addClass('invalid');
-//   }
-
-//   //validate capital letter
-//   if ( pswd.match(/[A-Z]/) ) {
-//       $('#capital').removeClass('invalid').addClass('valid');
-//   } else {
-//       $('#capital').removeClass('valid').addClass('invalid');
-//   }
-
-//   //validate number
-//   if ( pswd.match(/\d/) ) {
-//       $('#number').removeClass('invalid').addClass('valid');
-//   } else {
-//       $('#number').removeClass('valid').addClass('invalid');
-//   }
-//   if (/^[a-zA-Z0-9- ]*$/.test(pswd) == false) {
-//       $('#special').removeClass('invalid').addClass('valid');
-//   } else {
-//       $('#special').removeClass('valid').addClass('invalid');
-//   }
-
-//   }).focus(function() {
-//       $('#pswd_info').show();
-//   }).blur(function() {
-//       $("#pswd_info").hide();
-
-//   });
-// });
-// $('#password_confirmation').on('click',function(){
-//  $('#pswd_info').hide();
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// password popup container 
-
-
 let myInput = document.getElementById("password"); 
 let letter = document.getElementById("letter");
 let capital = document.getElementById("capital");
@@ -168,56 +57,26 @@ myInput.onfocus = function() {
   } 
 
 
+  // verifiy password 
+
+  function verifyPassword() { 
+    let pass1 = document.getElementById('password').value; 
+    let pass2 = document.getElementById('confirm-password').value; 
+
+    let match = true; 
+
+    if (pass1 !== pass2) { 
+        alert('Passwords Do not match'); 
+        document.getElementById('password').style.borderColor = "#ff0000";
+        document.getElementById('confirm-password').style.borderColor = "#ff0000";
+        match = false; 
+    } else { 
+        alert("Passwords match."); 
+    }
+    return match; 
+}
 
 
-
-
-// text underneath password field 
-
-
-// password.addEventListener('focusin', () => {
-//     if (password.classList.contains('invalid-input')) {
-//     password.classList.remove('invalid-input');
-//     }
-// passwordValidation.classList.add('pass-focus');
-// })
-
-
-
-// password.addEventListener('input', () => {
-//     const minEightcharCheck = document.querySelector('.pass-at-least-eight');
-//     const minOneNumCheck = document.querySelector('.pass-at-least-num');
-//     const minOneCharCheck = document.querySelector('.pass-at-least-char');
-
-//     if (password.value.trim().length >= 8) {
-//         minEightcharCheck.textContent = '✓';
-//         minEightcharCheck.classList.add('pass');
-//     } else {
-//         minEightcharCheck.textContent = '🗴';
-//         minEightcharCheck.classList.remove('pass');
-//     }
-
-//     const hasNum = /\d/.test(password.value);
-//     if (hasNum) {
-//         minOneNumCheck.textContent = '✓';
-//         minOneNumCheck.classList.add('pass');
-//     } else {
-//         minOneNumCheck.textContent = '🗴';
-//         minOneNumCheck.classList.remove('pass');
-
-//     }
-
-//     const hasChar = /[a-zA-Z]/.test(password.value);
-//     if (hasChar) {
-//         minOneCharCheck.textContent = '✓';
-//         minOneCharCheck.classList.add('pass');
-
-//     } else {
-//         minOneCharCheck.textContent = '🗴';
-//         minOneCharCheck.classList.remove('pass');
-
-//     }
-// }) 
 
 
 
